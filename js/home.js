b8409f67fbd80ad3ba37a2c010778edfe2e694a2
toledorp <roge.pupo.toledo@gmail.com>
2026-03-16 15:58:37 -0300

    feat: render characters list from SWAPI

--- js/home.js
+++ js/home.js
@@ -1,16 +1,32 @@
 console.log("Home page loaded");
 
 async function loadCharacters() {
+
     try {
 
         const response = await fetch("https://swapi.dev/api/people/");
         const data = await response.json();
 
-        console.log("Characters:", data.results);
+        const charactersList = document.getElementById("charactersList");
+
+        charactersList.innerHTML = "";
+
+        data.results.forEach(character => {
+
+            const characterElement = document.createElement("p");
+
+            characterElement.textContent = character.name;
+
+            charactersList.appendChild(characterElement);
+
+        });
 
     } catch (error) {
+
         console.error("Error fetching characters:", error);
+
     }
+
 }
 
 loadCharacters();
